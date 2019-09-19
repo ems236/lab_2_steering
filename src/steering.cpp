c748731a24822eec7dce108dc0160462d5eac40f
ems236 <ems236@case.edu>
2019-09-18 20:12:56 -0400

    updated debug info and fixed bookkeeping bugs

--- src/steering.cpp
+++ src/steering.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <vector>
 #include <math.h>
+#include <sstream>
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 #include "std_msgs/Float64.h"
@@ -14,8 +15,8 @@
 using namespace std;
 using geometry_msgs::Twist;
 
-const string WRITE_VELOCITY_TOPIC = "cmd_vel";
-const string READ_LASER_TOPIC = "laser_1";
+const string WRITE_VELOCITY_TOPIC = "/robot1/cmd_vel";
+const string READ_LASER_TOPIC = "/robot1/laser_1";
 
 //I don't really know how to find this so it's hardcoded
 //added a little padding
@@ -53,6 +54,8 @@ string parse_desired_topic(int argc, char** argv)
         }
     }
     
+    ROS_INFO("Parsed %s as the desired velocity topic", topic_name.c_str());
+    
     return topic_name;
 }
 
@@ -61,6 +64,8 @@ void desired_velocity_callback(const Twist::ConstPtr& desired_velocity_twist)
 {
     desired_velocity = desired_velocity_twist->linear.x;
     desired_angular_velocity = desired_velocity_twist->angular.z;
+    
+    ROS_INFO("Receiving des vel info %f %f", desired_velocity, desired_angular_velocity);
 }
 
 //records info and exits
@@ -73,6 +78,18 @@ void laser_callback(const sensor_msgs::LaserScan::ConstPtr& laser_scan)
     
     current_ranges.clear();
     current_ranges.insert(current_ranges.end(), &(laser_scan->ranges)[0], &(laser_scan->ranges)[size]);
+    
+    start_angle = laser_scan->angle_min;
+    angle_step = laser_scan->angle_increment;
+    
+    //ROS_INFO("Receiving laser info");
+    /*float current_angle = start_angle;
+    for(vector<float>::iterator current = current_ranges.begin(); current != current_ranges.end(); ++current)
+    {
+        ROS_INFO("Laser info: angle: %f distance: %f", current_angle, *current);
+       
+        current_angle += angle_step;
+    }*/
 }
 
 int sign(double value)
@@ -116,8 +133,8 @@ bool can_move_direction(double linear, double angular)
     // y change = r - rcos theta
     // x change = r - rsin theta
 
-    //Calculate based on 1 second, which might be a little conservative.
-    double time = 1.0;
+    //might be a little conservative.
+    double time = 0.5;
     
     if(angular == 0)
     {
@@ -237,14 +254,23 @@ Twist avoid_obstacle_velocity()
     return twist_for(0, PI / 2);
 }
 
-void steer()
+void publish_new_velocity(Twist new_vel, ros::Publisher* velocity_publisher)
+{
+    current_velocity = new_vel.linear.x;
+    current_angular_velocity = new_vel.angular.z;
+    velocity_publisher->publish(new_vel);
+}
+
+void steer(ros::Publisher* velocity_publisher)
 {
     const int SPEED_STEP = 0.05;
     if(can_move_desired())
     {
         if(!is_current_desired())
         {
-            //publish new twist; 
+            //publish new twist;
+            ROS_INFO("Setting velocity to current desired");
+            publish_new_velocity(twist_for(desired_velocity, desired_angular_velocity), velocity_publisher)
         }
         
         return;
@@ -252,34 +278,41 @@ void steer()
     
     double new_speed = current_velocity + SPEED_STEP;
     //prioritize not spinning in place
-    if(can_move_straight(current_velocity) && can_move_straight(new_speed))
-    {
-        //update speed
-    }
+   
     
     if(can_move_current())
     {
-       //try and speed up because that's fun
-       if(can_move_direction(new_speed, current_angular_velocity))
-       {
+        //try and speed up because that's fun
+        if(can_move_direction(new_speed, current_angular_velocity))
+        {
+            ROS_INFO("Increasing speed");
             //update speed;
-       }
-       return; 
+            publish_new_velocity(twist_for(new_speed, velocity_publisher));
+        }
+       
+        else if(can_move_straight(current_velocity) && can_move_straight(new_speed))
+        {
+            ROS_INFO("Moving in a straight line");
+            //update speed
+            publish_new_velocity(twist_for(new_speed, 0), velocity_publisher);
+        }
+        
+        return; 
     }
     
     //make up a new velocity
+    ROS_INFO("Avoiding an obstacle");
     Twist new_dir = avoid_obstacle_velocity();
+    publish_new_velocity(new_dir, velocity_publisher);
 }
-
-
 
 int main(int argc, char** argv)
 {
     ros::init(argc, argv, "lab_2_steering");
     
     string read_velocity_topic = parse_desired_topic(argc, argv);
-    ROS_INFO("Reading velocity from topic %s\n", read_velocity_topic);
-    ROS_INFO("Writing velocity to topic %s\n", WRITE_VELOCITY_TOPIC);
+    ROS_INFO("Reading velocity from topic %s\n", read_velocity_topic.c_str());
+    ROS_INFO("Writing velocity to topic %s\n", WRITE_VELOCITY_TOPIC.c_str());
     
     //Let launch file handle remapping
     //Would be cool to print the remapping info
@@ -289,12 +322,15 @@ int main(int argc, char** argv)
     ros::Subscriber velocity_subscriber = node_handle.subscribe(read_velocity_topic, 200, desired_velocity_callback);
     ros::Subscriber laser_subscriber = node_handle.subscribe(READ_LASER_TOPIC, 1000, laser_callback);
     
+    ros::Rate loop_rate(10);
     //Main loop
     while(ros::ok())
     {
+        steer(&velocity_publisher);
         
         //process all callbacks
         ros::spinOnce();
+        loop_rate.sleep();
     }
     
     return 0;   
